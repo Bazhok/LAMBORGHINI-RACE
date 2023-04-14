5181d522c73b286f23f52e763387d92b0e315b6d
Bazhok <97741579+Bazhok@users.noreply.github.com>
2023-04-14 14:05:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,169 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.onclick = () => {
+    menuMobile.classList.toggle('menu--open');
+};
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+   
+    loop: true,
+    
+   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-right', /*переименовала из дефолтного значения swiper-button-next, что позволило убрать дефолтные синие стрелки */ 
+      prevEl: '.swiper-button-left',/*переименовала из дефолтного значения swiper-button-prev */ 
+    },
+  breakpoints:{
+    0: {
+        slidesPerView: 1,   
+    },
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 20,  
+    }
+  }
+   
+  });
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.23198165526408, lng: -8.628079252421779},
+    zoom: 14,
+    styles:[
+      {
+          "featureType": "administrative",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.province",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 65
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": "50"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "30"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "40"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ffff00"
+              },
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -97
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -100
+              }
+          ]
+      }
+  ]
+  });
+}
+
+window.initMap = initMap;
